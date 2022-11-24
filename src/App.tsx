3b24ee5bd79ecb6b29638692b23dd4730d39831a
gusleaooliveira <gus.leaono@gmail.com>
2022-11-23 21:26:49 -0300

    p

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 function App() {
   return (
-    <button
-      onClick={() => {
-        alert("Exemplo inicializado com sucesso!");
-      }}
-    >
-      Clique!
-    </button>
+    <>
+      <button
+        onClick={() => {
+          alert("Exemplo inicializado com sucesso!");
+        }}
+      >
+        Clique!
+      </button>
+      <p>Clique no botão e veja a mágica acontecer!</p>
+    </>
   );
 }
 
